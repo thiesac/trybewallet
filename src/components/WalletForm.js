6f1d73db2b99bbaee74c1e9eb8940132a8bf2aae
Thiesa <thiesa.c@gmail.com>
2023-05-13 09:39:05 -0400

    Inicianto WalletForm com thunk

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,32 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <div>
+        <h3>WalletForm</h3>
+        <div>
+          <label htmlFor="input-value">
+            Valor:
+            <input
+              data-testid="value-input"
+              type="number"
+              id="input-value"
+            />
+          </label>
+          <label htmlFor="input-description">
+            Descrição:
+            <input
+              data-testid="description-input"
+              type="text"
+              id="input-description"
+            />
+          </label>
+          <label>
+            <select data-testid="currency-input">
+              {/* <option></option> */}
+            </select>
+          </label>
+        </div>
+      </div>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import { connect } from 'react-redux';
 import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <h2>Wallet</h2>
+        <WalletForm />
         <Header />
       </div>
     );
