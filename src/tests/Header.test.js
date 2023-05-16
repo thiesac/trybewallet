9a7a1b36304920202055558d385489345ee7efce
Thiesa <thiesa.c@gmail.com>
2023-05-15 20:30:45 -0400

    Requisito 5 passando localmente

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,7 +1,20 @@
-// import Header from "../components/Header";
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+import App from '../App';
 
-// test('', () => {
+describe('Testa o componente Header', () => {
+  test('Testa se os elementos do Header são carregados corretamente', async () => {
+    renderWithRouterAndRedux(<App />);
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/senha/i);
+    const button = screen.getByRole('button', { name: /entrar/i });
 
-// })
+    userEvent.type(emailInput, 'email@email.com');
+    userEvent.type(passwordInput, '123456');
+    userEvent.click(button);
+
+    screen.getByText(/email@email\.com/i);
+  });
+});
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import {  screen } from '@testing-library/react';
-import { renderWithRedux } from './helpers/renderWith';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
 import App from '../App';
 
 describe('Testa o componente Login', () => {
   test('Testa se todos os componentes do Login são carregados corretamente', () => {
-    renderWithRedux(<App />);
-    screen.getByRole('heading', { name: /login/ });
+    renderWithRouterAndRedux(<App />);
+    screen.getByRole('heading', { name: /login/i });
+    screen.getByText(/email/i);
+    screen.getByRole('textbox', { name: /email/i });
+    screen.getByText(/senha/i);
+    screen.getByLabelText(/senha/i);
+    screen.getByRole('button', { name: /entrar/i });
+  });
+
+  test('Testa se o botão /entrar/ só é liberado quando os campos são preenchidos corretamente', () => {
+    renderWithRouterAndRedux(<App />);
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/senha/i);
+    const button = screen.getByRole('button', { name: /entrar/i });
+
+    userEvent.type(emailInput, 'email@email');
+    userEvent.type(passwordInput, '123456');
+    expect(button).toHaveAttribute('disabled');
   });
 });
